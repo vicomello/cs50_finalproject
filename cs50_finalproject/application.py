078e84c1f570c00610896281ff1ea899580a935a
Victoria Oldemburgo de Mello <victoriademello@college.harvard.edu>
2018-12-06 07:19:05 +0000

    page with interests working

--- cs50_finalproject/application.py
+++ cs50_finalproject/application.py
@@ -96,8 +96,15 @@ def personality():
         session["user_id"]
 
     if request.method == "POST":
+        #extraversion = int(request.form.get("EXT1")) + int(request.form.get("EXT3")) + int(request.form.get("EXT5")) +  int(request.form.get("EXT7")) +  int(request.form.get("EXT9")) - int(request.form.get("EXT2")) -  int(request.form.get("EXT4")) -  int(request.form.get("EXT6")) -  int(request.form.get("EXT8")) - int(request.form.get("EXT10"))
+        extraversion = int(request.form.get("EXT1")) + int(request.form.get("EXT3")) + int(request.form.get("EXT5")) +  int(request.form.get("EXT7")) + int(request.form.get("EXT9")) - int(request.form.get("EXT2")) - int(request.form.get("EXT4")) - int(request.form.get("EXT6")) - int(request.form.get("EXT8")) - int(request.form.get("EXT10"))
+        agreeableness = int(request.form.get("AGR2")) + int(request.form.get("AGR4")) + int(request.form.get("AGR6")) +  int(request.form.get("AGR8")) +  int(request.form.get("AGR9")) + int(request.form.get("AGR10")) - int(request.form.get("AGR1")) -  int(request.form.get("AGR3")) - int(request.form.get("AGR5")) - int(request.form.get("AGR7"))
+        consciousness = int(request.form.get("CSN1")) + int(request.form.get("CSN3")) + int(request.form.get("CSN5")) +  int(request.form.get("CSN7")) +  int(request.form.get("CSN9")) + int(request.form.get("CSN10")) - int(request.form.get("CSN2")) -  int(request.form.get("CSN4")) - int(request.form.get("CSN6")) - int(request.form.get("CSN8"))
+        estability = int(request.form.get("EST2")) + int(request.form.get("EST4")) - int(request.form.get("EST1")) - int(request.form.get("EST3")) - int(request.form.get("EST5")) - int(request.form.get("EST6")) - int(request.form.get("EST7")) - int(request.form.get("EST8")) - int(request.form.get("EST9")) - int(request.form.get("EST10"))
+        openess = int(request.form.get("OPN1")) + int(request.form.get("OPN3")) + int(request.form.get("OPN5")) + int(request.form.get("OPN7")) +  int(request.form.get("OPN8")) + int(request.form.get("OPN9")) + int(request.form.get("OPN10")) - int(request.form.get("OPN2")) - int(request.form.get("OPN4")) -  int(request.form.get("OPN6"))
+
         db.execute("INSERT INTO traits (EXT, AGR, CON, EST, OPN, user_id) VALUES (:EXT, :AGR, :CON, :EST, :OPN, :user_id)",
-        EXT=request.form.get("EXT1"), AGR=request.form.get("AGR1"), CON=request.form.get("CSN1"), EST=request.form.get("EST1"), OPN=request.form.get("OPN1"), user_id=session["user_id"])
+        EXT=extraversion, AGR=agreeableness, CON=consciousness, EST=estability, OPN=openess, user_id=session["user_id"])
     return apology("deu certo sim")
 
 
@@ -107,6 +114,16 @@ def personality():
 #    if request.method == "GET":
 #        return render_template("personality_results.html")
 
+@app.route("/lifestyle", methods=["GET", "POST"])
+@login_required
+def lifestyle():
+    if request.method == "GET":
+        return render_template("lifestyle.html")
+        session["user_id"]
+
+    if request.method == "POST":
+        db.execute("INSERT INTO traits (lf1, lf2, lf3, lf4, lf5, lf6, lf7, lf8, lf9, lf10, lf11, lf12, lf13, lf14, lf15) VALUES (:lf1, :lf2, :lf3, :lf4, :lf5, :lf6, :lf7, :lf8, :lf9, :lf10, :lf11, :lf12, :lf13, :lf14, :lf15",
+        lf1=request.form.get("lf1"), lf2=request.form.get("lf2"), lf3=request.form.get("lf3"), lf4=request.form.get("lf4"), lf5=request.form.get("lf5"), lf6=request.form.get("lf6"), lf7=request.form.get("lf7"), lf8=request.form.get("lf8"), lf9=request.form.get("lf9"), lf10=request.form.get("lf10"), lf11=request.form.get("lf11"), lf12=request.form.get("lf12"), lf13=request.form.get("lf13"), lf14=request.form.get("lf14"), lf15=request.form.get("lf15"))
 
 @app.route("/index", methods=["GET", "POST"])
 @login_required
